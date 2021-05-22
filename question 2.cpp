a43cd51e4f92b2e4d698ae5ce86b3a3d712e0d0b
keertanapandit <75423482+keertanapandit@users.noreply.github.com>
2021-05-22 12:32:25 +0530

    Add files via upload

---
+++ question 2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double a,b,product;
+	cout<<"enter two numbers:";
+	cin>>a>>b;
+	product=a*b;
+	cout<<"the product is:"<<product;
+	return 0;
+}
